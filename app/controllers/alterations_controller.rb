b32e8611293719abd067e2bc52b021bdc6e40aa0
Epic91 <hernandeze791@gmail.com>
2021-01-26 21:53:05 -0600

    completed alteration forms

--- app/controllers/alterations_controller.rb
+++ app/controllers/alterations_controller.rb
@@ -1,10 +1,6 @@
 class AlterationsController < ApplicationController
   before_action :set_alteration, only: %i[ show edit update destroy ]
 
-  # GET /alterations or /alterations.json
-  def index
-    @alterations = Alteration.all
-  end
 
   # GET /alterations/1 or /alterations/1.json
   def show
@@ -18,35 +14,21 @@ class AlterationsController < ApplicationController
 
   # GET /alterations/1/edit
   def edit
+    @alteration = Alteration.find(params[:id])
   end
 
   # POST /alterations or /alterations.json
   def create
-    #items = ["blouse", "pants", "skirts"]
     @alteration = Alteration.new(alteration_params)
-
-    respond_to do |format|
-      if @alteration.save
-        format.html { redirect_to @alteration, notice: "Alteration was successfully created." }
-        format.json { render :show, status: :created, location: @alteration }
-      else
-        format.html { render :new, status: :unprocessable_entity }
-        format.json { render json: @alteration.errors, status: :unprocessable_entity }
-      end
-    end
+    @alteration.save
+    redirect_to alteration_path(@alteration)
   end
 
   # PATCH/PUT /alterations/1 or /alterations/1.json
   def update
-    respond_to do |format|
-      if @alteration.update(alteration_params)
-        format.html { redirect_to @alteration, notice: "Alteration was successfully updated." }
-        format.json { render :show, status: :ok, location: @alteration }
-      else
-        format.html { render :edit, status: :unprocessable_entity }
-        format.json { render json: @alteration.errors, status: :unprocessable_entity }
-      end
-    end
+    @alteration = Alteration.find(params[:id])
+    @alteration.update(alteration_params)
+    redirect_to alteration_path(@alteration)
   end
 
   # DELETE /alterations/1 or /alterations/1.json
